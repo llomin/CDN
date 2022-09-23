986441d097809f7790199420df2ddc6c9756c8fc
llomin <57883353+llomin@users.noreply.github.com>
2022-09-23 15:52:35 +0800

    Update live2d url

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://llomin.gitee.io/live2d/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/llomin/CDN/live2d/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
