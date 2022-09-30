35d9dee1e5dc1130ae0133e92654fc08d9193241
llomin <57883353+llomin@users.noreply.github.com>
2022-09-30 15:10:49 +0800

    Create random-home-img.js

---
+++ js/random-home-img.js
@@ -0,0 +1,8 @@
+const index_imgs = [
+    'https://bu.dusays.com/2022/09/23/632dd5472a947.jpg',
+    'https://bu.dusays.com/2022/09/30/633684ae61e8c.jpg',
+    'https://bu.dusays.com/2022/09/30/63368625ea43b.jpg'
+]
+
+var index_img = index_imgs[Math.floor(Math.random() * index_imgs.length)]
+document.getElementsByClassName("full_page").style.backgroundImage = "url(" + index_img + ")"
