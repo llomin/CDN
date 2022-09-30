f44282767adeb4569ed49d414842d3c2d9a955b1
llomin <57883353+llomin@users.noreply.github.com>
2022-09-30 15:45:16 +0800

    Update random-home-img.js

--- js/random-home-img.js
+++ js/random-home-img.js
@@ -4,5 +4,9 @@ const index_imgs = [
     'https://bu.dusays.com/2022/09/30/63368625ea43b.jpg'
 ]
 
-var index_img = index_imgs[Math.floor(Math.random() * index_imgs.length)]
-document.querySelector(".full_page").style.backgroundImage = "url(" + index_img + ")"
+var index_img_dom = document.querySelector(".full_page")
+let index_img
+if (index_img_dom) {
+    index_img = index_imgs[Math.floor(Math.random() * index_imgs.length)]
+    index_img_dom.style.backgroundImage = "url(" + index_img + ")"
+}
